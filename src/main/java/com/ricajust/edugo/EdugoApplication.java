bbab29a66c750299d71ec209c984e5a0f75992a4
ricarjust <ricartoonstudio@gmail.com>
2025-01-19 23:30:33 -0300

    feat: first commit

---
+++ src/main/java/com/ricajust/edugo/EdugoApplication.java
@@ -0,0 +1,13 @@
+package com.ricajust.edugo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdugoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdugoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricajust/edugo/EdugoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricajust.edugo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdugoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
